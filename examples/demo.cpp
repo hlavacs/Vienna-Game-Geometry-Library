a7f9a13c9f2238f57c8317bee8b2077ad1510a0d
athurner10-lgtm <athurner10@gmail.com>
2026-04-23 17:09:17 +0200

    chore: add vulkan dependancy and small test demo

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -1,6 +1,8 @@
 #include <vgeo/CollisionSystem.hpp>
+#include <vgeo/internal/gpu/GpuBackend.hpp>
 
 int main() {
     vgeo::CollisionSystem system;
+    vgeo::GpuBackend backend;
     return 0;
 }
---
+++ vgeo/internal/gpu/GpuBackend.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <print>
+
+namespace vgeo {
+
+class GpuBackend{
+    
+    public:
+        GpuBackend();
+};
+
+}  
