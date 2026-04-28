00f3bb6c0fb0b95af1a1844b4076260fc0232159
Fabian Haßlacher <fabian.hasslacher@proton.me>
2026-04-28 23:56:32 +0200

    chore: clean up demo

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -1,8 +1,6 @@
 #include <vgeo/CollisionSystem.hpp>
-#include <vgeo/internal/gpu/GpuBackend.hpp>
 
 int main() {
     vgeo::CollisionSystem system;
-    vgeo::internal::GpuBackend backend;
     return 0;
 }
