4fcb38a6d5580e8fe00d007d5ed3ce1b6e9fbc82
Fabian Haßlacher <fabian.hasslacher@proton.me>
2026-05-01 02:17:17 +0200

    feat: implement ConvexHull shape

--- vgeo/internal/cpu/shapes/ConvexHull.hpp
+++ vgeo/internal/cpu/shapes/ConvexHull.hpp
@@ -32,16 +32,37 @@ public:
         return m_indices;
     }
 
-    [[nodiscard]] Bv computeBv() const {
-        return {{0, 0, 0}, {0, 0, 0}};
+    [[nodiscard]] Bv computeBv() const
+        requires std::same_as<Bv, Aabb> {
+        Terathon::Point3D min = m_vertices[0];
+        Terathon::Point3D max = m_vertices[0];
+        for (const Terathon::Point3D& vertex : m_vertices) {
+            min = Terathon::Point3D{std::min(min.x, vertex.x), std::min(min.y, vertex.y), std::min(min.z, vertex.z)};
+            max = Terathon::Point3D{std::max(max.x, vertex.x), std::max(max.y, vertex.y), std::max(max.z, vertex.z)};
+        }
+        return {min, max};
     }
 
     [[nodiscard]] Terathon::Point3D centroid() const {
-        return {0, 0, 0};
+        Terathon::Vector3D sum{0.0f, 0.0f, 0.0f};
+        for (const Terathon::Point3D& vertex : m_vertices) {
+            sum += vertex;
+        }
+        float invCount = 1.0f / static_cast<float>(m_vertices.size());
+        return {sum.x * invCount, sum.y * invCount, sum.z * invCount};
     }
 
-    [[nodiscard]] Terathon::Point3D support(Terathon::Vector3D) const {
-        return {0, 0, 0};
+    [[nodiscard]] Terathon::Point3D support(Terathon::Vector3D dir) const {
+        Terathon::Point3D furthest = m_vertices[0];
+        float maxDot = Terathon::Dot(dir, furthest);
+        for (const Terathon::Point3D& vertex : m_vertices) {
+            float dot = Terathon::Dot(dir, vertex);
+            if (dot > maxDot) {
+                maxDot = dot;
+                furthest = vertex;
+            }
+        }
+        return furthest;
     }
 
 private:
