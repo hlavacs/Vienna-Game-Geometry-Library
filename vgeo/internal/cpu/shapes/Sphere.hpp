9a21bc48f1696713dd4a2d1f2db68d8df7fa0bf7
Fabian Haßlacher <fabian.hasslacher@proton.me>
2026-05-01 02:56:18 +0200

    feat: implement Sphere shape

--- vgeo/internal/cpu/shapes/Sphere.hpp
+++ vgeo/internal/cpu/shapes/Sphere.hpp
@@ -4,38 +4,52 @@
 #include "vgeo/internal/cpu/BoundingVolume.hpp"
 #include "vgeo/internal/cpu/shapes/CollisionShape.hpp"
 
+#include <TSConformal3D.h>
 #include <TSVector3D.h>
+
+#include <cmath>
+#include <concepts>
 
 namespace vgeo::internal::cpu {
 
 template <BoundingVolume Bv>
 class Sphere {
 public:
-    Sphere(Terathon::Point3D center, float radius) : m_center{center}, m_radius{radius} {}
+    Sphere(Terathon::Point3D center, float radius)
+        : m_sphere{-1.0f, center.x, center.y, center.z, -(Terathon::SquaredMag(center) - radius * radius) * 0.5f} {}
+
+    [[nodiscard]] Terathon::Sphere3D getSphere() const {
+        return m_sphere;
+    }
 
     [[nodiscard]] Terathon::Point3D getCenter() const {
-        return m_center;
+        return {m_sphere.x, m_sphere.y, m_sphere.z};
     }
 
     [[nodiscard]] float getRadius() const {
-        return m_radius;
+        return std::sqrt(Terathon::SquaredRadiusNorm(m_sphere));
     }
 
-    [[nodiscard]] Bv computeBv() const {
-        return {{0, 0, 0}, {0, 0, 0}};
+    [[nodiscard]] Bv computeBv() const
+        requires std::same_as<Bv, Aabb> {
+        const float radius = getRadius();
+        return {
+            {m_sphere.x - radius, m_sphere.y - radius, m_sphere.z - radius},
+            {m_sphere.x + radius, m_sphere.y + radius, m_sphere.z + radius},
+        };
     }
 
     [[nodiscard]] Terathon::Point3D centroid() const {
-        return {0, 0, 0};
+        return getCenter();
     }
 
-    [[nodiscard]] Terathon::Point3D support(Terathon::Vector3D) const {
-        return {0, 0, 0};
+    [[nodiscard]] Terathon::Point3D support(Terathon::Vector3D dir) const {
+        const float radius = getRadius();
+        return {m_sphere.x + radius * dir.x, m_sphere.y + radius * dir.y, m_sphere.z + radius * dir.z};
     }
 
 private:
-    Terathon::Point3D m_center;
-    float m_radius;
+    Terathon::Sphere3D m_sphere;
 };
 
 static_assert(CollisionShape<Sphere<Aabb>>);
