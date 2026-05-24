6a95eb5ed2c32f2a76ff971eaf83f5a070d2a3fd
Fabian Haßlacher <fabian.hasslacher@proton.me>
2026-05-24 03:09:32 +0200

    style: reorder members in BoundingVolume concept

--- vgeo/internal/cpu/Aabb.hpp
+++ vgeo/internal/cpu/Aabb.hpp
@@ -23,6 +23,17 @@ public:
         return m_max;
     }
 
+    [[nodiscard]] static Aabb merge(const Aabb& a, const Aabb& b) {
+        return {
+            {std::min(a.m_min.x, b.m_min.x), std::min(a.m_min.y, b.m_min.y), std::min(a.m_min.z, b.m_min.z)},
+            {std::max(a.m_max.x, b.m_max.x), std::max(a.m_max.y, b.m_max.y), std::max(a.m_max.z, b.m_max.z)},
+        };
+    }
+
+    [[nodiscard]] Terathon::Point3D centroid() const {
+        return {(m_min.x + m_max.x) * 0.5f, (m_min.y + m_max.y) * 0.5f, (m_min.z + m_max.z) * 0.5f};
+    }
+
     [[nodiscard]] bool overlaps(const Aabb& other) const {
         return m_min.x <= other.m_max.x && m_max.x >= other.m_min.x && m_min.y <= other.m_max.y &&
                m_max.y >= other.m_min.y && m_min.z <= other.m_max.z && m_max.z >= other.m_min.z;
@@ -30,17 +41,6 @@ public:
 
     [[nodiscard]] bool intersectsRay(Terathon::Point3D origin, Terathon::Vector3D dir) const {
         return false;
-    }
-
-    [[nodiscard]] Terathon::Point3D centroid() const {
-        return {(m_min.x + m_max.x) * 0.5f, (m_min.y + m_max.y) * 0.5f, (m_min.z + m_max.z) * 0.5f};
-    }
-
-    [[nodiscard]] static Aabb merge(const Aabb& a, const Aabb& b) {
-        return {
-            {std::min(a.m_min.x, b.m_min.x), std::min(a.m_min.y, b.m_min.y), std::min(a.m_min.z, b.m_min.z)},
-            {std::max(a.m_max.x, b.m_max.x), std::max(a.m_max.y, b.m_max.y), std::max(a.m_max.z, b.m_max.z)},
-        };
     }
 
 private:
--- vgeo/internal/cpu/BoundingVolume.hpp
+++ vgeo/internal/cpu/BoundingVolume.hpp
@@ -8,10 +8,10 @@ namespace vgeo::internal::cpu {
 
 template <typename Bv>
 concept BoundingVolume = requires(const Bv bv, const Bv other, Terathon::Point3D origin, Terathon::Vector3D dir) {
+    { Bv::merge(bv, other) } -> std::same_as<Bv>;
+    { bv.centroid() } -> std::same_as<Terathon::Point3D>;
     { bv.overlaps(other) } -> std::same_as<bool>;
     { bv.intersectsRay(origin, dir) } -> std::same_as<bool>;
-    { bv.centroid() } -> std::same_as<Terathon::Point3D>;
-    { Bv::merge(bv, other) } -> std::same_as<Bv>;
 };
 
 } // namespace vgeo::internal::cpu
