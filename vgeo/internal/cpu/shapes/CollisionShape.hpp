64ea5239417e22a63ae852e341e9807ba8d0191d
Fabian Haßlacher <fabian.hasslacher@proton.me>
2026-06-29 18:20:13 +0200

    refactor: require applyTransform in the CollisionShape concept

--- vgeo/internal/cpu/shapes/CollisionShape.hpp
+++ vgeo/internal/cpu/shapes/CollisionShape.hpp
@@ -3,6 +3,7 @@
 #include "vgeo/RayResult.hpp"
 #include "vgeo/internal/cpu/BoundingVolume.hpp"
 
+#include <TSMotor3D.h>
 #include <TSVector3D.h>
 
 #include <concepts>
@@ -11,13 +12,17 @@
 namespace vgeo::internal::cpu {
 
 template <typename S, typename Bv>
-concept CollisionShape =
-    BoundingVolume<Bv> &&
-    requires(const S shape, InstanceHandle handle, Terathon::Vector3D dir, Terathon::Point3D origin) {
-        { shape.template computeBv<Bv>() } -> std::same_as<Bv>;
-        { shape.centroid() } -> std::same_as<Terathon::Point3D>;
-        { shape.support(dir) } -> std::same_as<Terathon::Point3D>;
-        { shape.intersectRay(handle, origin, dir) } -> std::same_as<std::optional<RayHit>>;
-    };
+concept CollisionShape = BoundingVolume<Bv> && requires(const S                  shape,
+                                                        InstanceHandle           handle,
+                                                        Terathon::Vector3D       dir,
+                                                        Terathon::Point3D        origin,
+                                                        const Terathon::Motor3D& motor,
+                                                        real                     scale) {
+    { shape.template computeBv<Bv>() } -> std::same_as<Bv>;
+    { shape.centroid() } -> std::same_as<Terathon::Point3D>;
+    { shape.support(dir) } -> std::same_as<Terathon::Point3D>;
+    { shape.intersectRay(handle, origin, dir) } -> std::same_as<std::optional<RayHit>>;
+    { shape.applyTransform(motor, scale) } -> std::same_as<S>;
+};
 
 } // namespace vgeo::internal::cpu
